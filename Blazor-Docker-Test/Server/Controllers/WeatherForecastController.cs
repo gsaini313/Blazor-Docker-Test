7f9b0d2625e653010017abb5500012ddc8ed348b
gsaini313 <38564615+gsaini313@users.noreply.github.com>
2024-02-14 13:19:14 -0500

    Update WeatherForecastController.cs

--- Blazor-Docker-Test/Server/Controllers/WeatherForecastController.cs
+++ Blazor-Docker-Test/Server/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace Blazor_Docker_Test.Server.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 15).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
